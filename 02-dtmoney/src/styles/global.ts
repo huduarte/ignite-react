7c1e617624eb710031e20df97d41244b40cec657
Hudson <hudsoneeto@outlook.com>
2021-03-25 07:28:29 -0300

    feat: uso de modal para cadastro de novas transaçoes e inicio de context API

--- 02-dtmoney/src/styles/global.ts
+++ 02-dtmoney/src/styles/global.ts
@@ -2,13 +2,14 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    -background: #f2f4f9;
+    --background: #f0f2f5;
     --red: #e52e4d;
-    --blue: #5429cc;
     --green: #33cc95;
+    --blue: #5429cc;
     --blue-light: #6933ff;
     
     --text-title: #363f4f;
+    --input-background: #e7e9ee;
     --text-body: #969cb3;
     --shape: #ffffff;
 }
@@ -30,7 +31,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: #f2f4f9;
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
@@ -50,5 +51,40 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+  .react-modal-overlay {
+    background: rgba(0,0,0, 0.5);
+
+    position: fixed;
+
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8)
+    }
   }
 `;

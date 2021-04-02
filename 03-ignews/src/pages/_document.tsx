8b7bd91d4fcf9b8469ede2c23d1fe66ceb3e2bf0
Hudson <hudsoneeto@outlook.com>
2021-04-02 15:26:12 -0300

    feat: componentes e paginas

--- 03-ignews/src/pages/_document.tsx
+++ 03-ignews/src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default class MyDocument extends Document{
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
         </Head>
         <body>
